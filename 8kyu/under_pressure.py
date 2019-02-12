896e4e44d32d19a91ae3ac3d761f354dec025072
Claudia Gázquez Alessio <claudia365x@gmail.com>
2019-02-12 11:16:35 +0100

    kata:under_pressure

---
+++ 8kyu/under_pressure.py
@@ -0,0 +1,10 @@
+def double_integer(i):
+    return i * 2
+    
+if __name__ == "__main__":
+    assert double_integer(2) = 4
+    assert double_integer(4) = 8
+    assert double_integer(3) = 6
+    assert double_integer(2.5) = 5
+    assert double_integer(7) = 14
+    assert double_integer(10) = 20
